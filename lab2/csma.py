57e538c1bb1381e250c4d7bd2568380516ed5e6c
hquan <henryquan.tum@gmail.com>
2019-07-02 18:49:47 -0400

    lab2

---
+++ lab2/csma.py
@@ -0,0 +1,21 @@
+# Lab 2
+
+class Node:
+
+	def __init__(self, position):
+		self.position = position
+		self.collision_count = 0
+
+
+	def transmit(self, node):
+		pass
+
+	def sense(self):
+		pass
+
+
+
+Tsim = 1000
+transmitted_packet_count = 0
+
+
